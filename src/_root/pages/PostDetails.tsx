75873c4af4e7a2d3fe9769b32bee78a4698ed7e1
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2023-12-15 23:41:16 +0100

    update

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -33,21 +33,7 @@ const PostDetails = () => {
   };
 
   return (
-    <div className="post_details-container">
-      <div className="hidden md:flex max-w-5xl w-full">
-        <Button
-          onClick={() => navigate(-1)}
-          variant="ghost"
-          className="shad-button_ghost">
-          <img
-            src={"/assets/icons/back.svg"}
-            alt="back"
-            width={24}
-            height={24}
-          />
-          <p className="small-medium lg:base-medium">Back</p>
-        </Button>
-      </div>
+    <div className="post_details-container h-screen">
 
       {isLoading || !post ? (
         <Loader />
